40f5fb6871d995e8763668d3ba2ed7fc18ffbef6
ffayesa <148739673+ffayesa@users.noreply.github.com>
2023-10-23 13:14:57 +0800

    Added browserView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { Link } from "react-router-dom"
+import { useEffect } from "react"
+import { useStudentsContext } from './hooks/useStudentsContext';
+import { StudentsContext } from './context/StudentsContext';
 
 // Pages and Components
 import Home from './pages/home';
 import { TopNavbar, SideNavbar } from './components/navbar';
 import { StudentView } from './pages/StudentView';
+import { BrowserView } from './pages/browserView';
 
 function App() {
+  const { students, dispatch } = useStudentsContext()
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      const response = await fetch('/api/students')
+      const json = await response.json()
+
+      if (response.ok) {
+          console.log("Database Response OK")
+        dispatch({type: 'SET_STUDENTS', payload: json})
+      } else {
+          console.log("Database Response NOT OK")
+      }
+    }
+
+    fetchStudents()
+  }, [dispatch])
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,7 +38,10 @@ function App() {
           <div className="content">
             <Routes>
               <Route path="/" element={<Home />}/>
-              <Route path="/students" element={<StudentView></StudentView>}/>
+              <Route path="/students" element={<div className="view"><StudentView/></div>}/>
+                  {students && students.map(student => (
+                    <Route path={"/students/"+student._id} element={<div className="view"><StudentView/><BrowserView student={student} key={student._id}/></div>}></Route>
+                  ))} 
             </Routes>
           </div>
       </div>
---
+++ frontend/src/pages/browserView.js
@@ -0,0 +1,21 @@
+import './../css/browserview.css'
+
+import { Link } from "react-router-dom"
+import { useStudentsContext } from "./../hooks/useStudentsContext"
+import { useEffect } from "react"
+import { StudentsContext } from "../context/StudentsContext"
+
+const BrowserView = ({student}) => {
+    return (
+        <div className="browser-view">
+            <div className="student-indicator">
+                <i className="fa-solid fa-user fa-2x"></i>  
+                <p className="side-title">Students / {student.studentName} ({student._id})</p>
+            </div>
+        </div>
+    )
+}
+
+export {
+    BrowserView
+}
