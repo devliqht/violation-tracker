9330f186f9819e6abec4e8ba2778e24bdf8fc0e5
devliqht <monapwc2020@gmail.com>
2023-10-24 00:52:22 +0800

    sum home page revisions

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,12 @@
+import './../css/home.css'
+
 const Home = () => {
     return (
         <div className="home">
-            <h2>Home</h2>
+            <h1 className="title">Welcome to the OSA Violation Tracker</h1>
+            <p>The OSA Violation Tracker is a program created by:</p>
+            <p><strong>Matt Erron G. Cabarrubias</strong>, <strong>Faye Akbar</strong></p>
+            <p>STEM 12 - ST</p>
         </div>
     )
 }
